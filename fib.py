f75f684f03952fabd4bd3c0ad20ee5201eba84fe
lsyleg <lsyleg@gmail.com>
2019-04-09 15:41:55 +0900

    Fibonacci Assignment

---
+++ fib.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/python
+
+def fib(n):
+        if n == 1 or n == 2:
+                return 1
+        else:
+                return fib(n - 1) + fib(n - 2)
+
+n = int(input("Input n = "))
+print(fib(n))
